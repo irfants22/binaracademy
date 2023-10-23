a45448b169a0a007b69695068c4e866db8a6cc0f
irfants22 <irfanee02@gmail.com>
2023-10-23 17:57:21 +0700

    add challenge chapter 5

---
+++ chapter-5/restfulapi-with-authentication/app/controllers/car.js
@@ -0,0 +1,122 @@
+const carService = require("./../services/car");
+
+const home = (req, res) => {
+    res.status(200).send("{ message: “Ping successfully” }");
+};
+
+const getList = async (req, res) => {
+    try {
+        const { category } = req.query;
+        const data = await carService.listCar(category);
+        res.json({
+            status: "OK",
+            message: "Success",
+            data,
+        });
+    } catch (error) {
+        res.status(error.statusCode).json({
+            status: "FAIL",
+            message: error.message,
+        });
+    }
+};
+
+const getDetail = async (req, res) => {
+    try {
+        const { id } = req.Cars;
+        const data = await carService.detailCar(id);
+        res.json({
+            status: "OK",
+            message: "Success",
+            data,
+        });
+    } catch (error) {
+        res.status(error.statusCode).json({
+            status: "FAIL",
+            message: error.message,
+        });
+    }
+};
+
+const create = async (req, res) => {
+    try {
+        const body = req.body;
+        const { id: userId } = req.user;
+        const data = await carService.createCar(body, userId);
+        res.json({
+            status: "OK",
+            message: "Success",
+            data,
+        });
+    } catch (error) {
+        res.status(error.statusCode).json({
+            status: "FAIL",
+            message: error.message,
+        });
+    }
+};
+
+const update = async (req, res) => {
+    try {
+        const { id } = req.Cars;
+        const body = req.body;
+        const { id: userId } = req.user;
+        const data = await carService.updateCar(id, body, userId);
+        res.json({
+            status: "OK",
+            message: "Success",
+            data,
+        });
+    } catch (error) {
+        res.status(error.statusCode).json({
+            status: "FAIL",
+            message: error.message,
+        });
+    }
+};
+
+const destroy = async (req, res) => {
+    try {
+        const { id } = req.Cars;
+        const { id: userId } = req.user;
+        await carService.deleteCar(id, userId);
+        res.json({
+            status: "OK",
+            message: "Delete Successfully",
+        });
+    } catch (error) {
+        res.status(error.statusCode).json({
+            status: "FAIL",
+            message: error.message,
+        });
+    }
+};
+
+function notFound(req, res) {
+    res.status(404).send({ message: "Page Not Found" });
+}
+
+const setValidationById = async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        const Cars = await carService.validation(id);
+        req.Cars = Cars;
+        next();
+    } catch (error) {
+        res.status(error.statusCode).json({
+            status: "FAIL",
+            message: error.message,
+        });
+    }
+};
+
+module.exports = {
+    home,
+    getList,
+    getDetail,
+    create,
+    update,
+    destroy,
+    notFound,
+    setValidationById,
+};
---
+++ chapter-5/restfulapi-with-authentication/index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+// const swaggerUi = require("swagger-ui-express");
+// const swaggerDocument = require("./docs");
+const {
+  home,
+  getList,
+  getDetail,
+  create,
+  update,
+  destroy,
+  notFound,
+  setValidationById,
+} = require("./app/controllers/car.js");
+const { register, registerAdmin, login, currentUser } = require("./app/controllers/user.js");
+const { authorize, isSuperAdmin, isSuperAdminOrAdmin } = require("./app/middlewares/auth.js")
+
+const PORT = 8000;
+
+app.use(express.json());
+
+// DOCUMENTATION API
+// app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// HOME
+app.get("/", home);
+
+// AUTHENTICATION
+app.post("/auth/login", login);
+app.post("/auth/register", register);
+app.post("/auth/register/admin", authorize, isSuperAdmin, registerAdmin);
+
+// USER
+app.get("/users/me", authorize, currentUser);
+
+// REST API ENDPOINTS
+app.get("/cars", authorize, getList);
+app.get("/cars/:id", authorize, setValidationById, getDetail);
+app.post("/cars", authorize, isSuperAdminOrAdmin, create);
+app.put("/cars/:id", authorize, isSuperAdminOrAdmin, setValidationById, update);
+app.delete("/cars/:id", authorize, isSuperAdminOrAdmin, setValidationById, destroy)
+
+// WRONG END POINT
+app.get("*", notFound);
+
+app.listen(PORT, () => {
+  console.log(`Server sudah berjalan pada http://127.0.0.1:${PORT}`);
+});
